7063112004d8088f835be4159b9e0ed52589e39d
Nicholas D. Socci <soccin@mskcc.org>
2021-02-17 17:57:20 -0500

    Fix git.describe; just use system call

--- gitTools.R
+++ gitTools.R
@@ -1,19 +1,25 @@
 
 git.describe<-function(){
 
-    suppressPackageStartupMessages(require(git2r))
+    system2(
+        "git",
+        c("describe", "--tags", "--always", "--long", "--dirty='-UNCOMMITED'"),
+        stdout=T
+        )
+
+    # suppressPackageStartupMessages(require(git2r))
 
     # system("git describe --always --dirty")
 
-    mostRecentTagIdx=rev(order(unlist(lapply(git2r::tags(),function(x){x$tagger$when$time}))))[1]
-    mostRecentTag=git2r::tags()[[mostRecentTagIdx]]
-    commits=git2r::commits()
+    # mostRecentTag=git2r::tags()[[1]]
+    # mostRecentTagName=names(git2r::tags())[1]
+    # commits=git2r::commits()
 
-    paste(
-        mostRecentTag$name,
-        which(lapply(commits,function(x){x$sha})==mostRecentTag$target),
-        paste0("g",substr(commits[[1]]$sha,1,7)),
-        sep="-"
-        )
+    # paste(
+    #     mostRecentTagName,
+    #     which(lapply(commits,function(x){x$sha})==mostRecentTag$sha),
+    #     paste0("g",substr(commits[[1]]$sha,1,7)),
+    #     sep="-"
+    #     )
 
 }
