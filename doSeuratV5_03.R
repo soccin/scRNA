624262efdd3d295aee4d21a4ddf5392fe2cf875d
Nicholas D. Socci <soccin@mskcc.org>
2021-04-30 23:00:27 -0400

    Gene marker plots

--- doSeuratV5_03.R
+++ doSeuratV5_03.R
@@ -43,14 +43,37 @@ suppressPackageStartupMessages({
 ##########################################################################
 
 obj=readRDS(args$PASS2.RDAFile)
-
-clusterRes="RNA_snn_res.0.5"
 s1=obj$s1
-maxClusters=nlevels(s1@meta.data[[clusterRes]])
-pal1=cols25(maxClusters)
 
-pg=DimPlot(s1, reduction = "umap", group.by="orig.ident", split.by="orig.ident", label.size=6,ncol=2) + scale_color_manual(values=c("darkviolet","red","grey")) + ggtitle(clusterRes)
+genes=scan("geneList.txt","")
+pg=FeaturePlot(s1,features=genes,combine=F)
+pg1=VlnPlot(s1,features=genes,combine=F,pt.size=.025)
 
-pdf(file=cc("seuratQC",args$PROJNAME,plotNo(),"UMAP","v2",".pdf"),width=11,height=8.5)
-print(pg)
+pgC=list()
+for(ii in seq(len(pg))) {
+    pgC[[len(pgC)+1]]=pg[[ii]]
+    pgC[[len(pgC)+1]]=pg1[[ii]]
+}
+pp=paginatePlots(pgC,2,2)
+pdf(file=cc("seuratQC",args$PROJNAME,plotNo(),"GenePlot","v2",".pdf"),width=11,height=8.5)
+print(pp)
 dev.off()
+
+genes1=intersect(rownames(obj$s1@assays$RNA@counts),genes)
+
+xx=t(as.matrix(obj$s1@assays$RNA@counts[genes1,]))
+rownames(xx)=map(rownames(xx),digest::digest,algo="md5") %>% unlist
+xx=xx[order(-rowSums(xx)),]
+
+
+
+# clusterRes="RNA_snn_res.0.5"
+# s1=obj$s1
+# maxClusters=nlevels(s1@meta.data[[clusterRes]])
+# pal1=cols25(maxClusters)
+
+# pg=DimPlot(s1, reduction = "umap", group.by="orig.ident", split.by="orig.ident", label.size=6,ncol=2) + scale_color_manual(values=c("darkviolet","red","grey")) + ggtitle(clusterRes)
+
+# pdf(file=cc("seuratQC",args$PROJNAME,plotNo(),"UMAP","v2",".pdf"),width=11,height=8.5)
+# print(pg)
+# dev.off()
