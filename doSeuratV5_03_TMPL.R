37fd9d9409cbe1700c9bb0173657355d1a17d484
Nicholas D. Socci <soccin@mskcc.org>
2021-04-30 22:10:16 -0400

    Added TEMPL version of 3 pass

---
+++ doSeuratV5_03_TMPL.R
@@ -0,0 +1,58 @@
+suppressPackageStartupMessages(require(stringr))
+
+usage="
+usage: doSeuratV5_02.R PARAMS.yaml
+
+    PARAMS.yaml     parameter file from pass1
+
+"
+
+cArgs=commandArgs(trailing=T)
+
+if(len(cArgs)!=1) {
+    cat(usage)
+    quit()
+}
+
+source("seuratTools.R")
+source("tools.R")
+source("doQCandFilter.R")
+
+library(yaml)
+args=read_yaml(cArgs[1])
+
+glbs=args$glbs
+ap=args$algoParams
+
+plotNo<-makeAutoIncrementor(20)
+
+suppressPackageStartupMessages({
+    library(Seurat)
+    library(patchwork)
+    library(tidyverse)
+    library(openxlsx)
+    library(pals)
+})
+
+
+
+##########################################################################
+#
+# INCLUDE BREAK
+#
+##########################################################################
+
+obj=readRDS(args$PASS2.RDAFile)
+
+
+
+# clusterRes="RNA_snn_res.0.5"
+# s1=obj$s1
+# maxClusters=nlevels(s1@meta.data[[clusterRes]])
+# pal1=cols25(maxClusters)
+
+# pg=DimPlot(s1, reduction = "umap", group.by="orig.ident", split.by="orig.ident", label.size=6,ncol=2) + scale_color_manual(values=c("darkviolet","red","grey")) + ggtitle(clusterRes)
+
+# pdf(file=cc("seuratQC",args$PROJNAME,plotNo(),"UMAP","v2",".pdf"),width=11,height=8.5)
+# print(pg)
+# dev.off()
