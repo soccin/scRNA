c3bca8155fc32d8b72ba003f7a64cd63b5898c5e
Nicholas D. Socci <soccin@mskcc.org>
2023-01-29 16:36:48 -0500

    Refactor:: funcs for get and plot qc tables

--- qcAndFilter.R
+++ qcAndFilter.R
@@ -17,6 +17,66 @@ get_mad3Cutoff<-function(xx,lower=T) {
     }
 }
 
+get_qc_tables <- function(md,filters) {
+
+    MIN_NCOUNT_RNA=filters$MIN_NCOUNT_RNA
+    MIN_FEATURE_RNA=filters$MIN_FEATURE_RNA
+    PCT_MITO=filters$PCT_MITO
+
+    tbl0=tibble(
+        Feature=c("nCount_RNA","nFeature_RNA","percent.mt"),
+        Cutoff=c(MIN_NCOUNT_RNA,MIN_FEATURE_RNA,PCT_MITO),
+        Method=c("Mad3-","Mad3-","Mad3+")
+    )
+
+    tbl1=md %>%
+        count(
+            Count.RNA=nCount_RNA>MIN_NCOUNT_RNA,
+            Num.Features=nFeature_RNA>MIN_FEATURE_RNA,
+            PCT.MT=percent.mt<PCT_MITO
+            ) %>%
+        mutate(PCT=round(100*n/sum(n),1))
+
+    nFail=c(
+        sum(md$nCount_RNA<=MIN_NCOUNT_RNA),
+        sum(md$nFeature_RNA<=MIN_FEATURE_RNA),
+        sum(md$percent.mt>=PCT_MITO)
+    )
+
+    pctFail=c(
+        mean(md$nCount_RNA<=MIN_NCOUNT_RNA),
+        mean(md$nFeature_RNA<=MIN_FEATURE_RNA),
+        mean(md$percent.mt>=PCT_MITO)
+    )
+
+    tbl2=tibble(
+        Feature=c("nCount_RNA","nFeature_RNA","percent.mt"),
+        N.Fail=nFail,PCT.Fail=pctFail
+    )
+
+
+    tbl3a=md %>% group_by(SampleID) %>% summarize(Fail.Feature_RNA=mean(nFeature_RNA<=MIN_FEATURE_RNA),Fail.Count_RNA=mean(nCount_RNA<=MIN_NCOUNT_RNA),Fail.Mito=mean(percent.mt>=PCT_MITO))
+    tbl3b=md %>% group_by(SampleID) %>% summarize(Fail.Feature_RNA=sum(nFeature_RNA<=MIN_FEATURE_RNA),Fail.Count_RNA=sum(nCount_RNA<=MIN_NCOUNT_RNA),Fail.Mito=sum(percent.mt>=PCT_MITO))
+
+    list(tbl0,tbl1,tbl2,tbl3a,tbl3b)
+
+}
+
+plot_qTables <- function(qTbls) {
+
+    ptb=list()
+    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(qTbls[[1]],is.numeric,~round(.,2)),rows=NULL))
+    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(qTbls[[2]],is.numeric,~round(.,2)),rows=NULL))
+    tt=qTbls[[3]] %>% mutate(PCT.Fail=round(PCT.Fail*100,2))
+    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tt,is.numeric,~round(.,2)),rows=NULL))
+    tt=qTbls[[4]] %>% mutate_if(is.numeric,\(x)x*100)
+    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tt,is.numeric,~round(.,2)),rows=NULL))
+    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(qTbls[[5]],is.numeric,~round(.,2)),rows=NULL))
+
+    ptb
+
+}
+
 qcSamples <- function(so) {
 
     sampleId=unique(so@meta.data$orig.ident)
@@ -43,53 +103,19 @@ qcSamples <- function(so) {
     MIN_FEATURE_RNA=get_mad3Cutoff(md$nFeature_RNA)
     PCT_MITO=get_mad3Cutoff(md$percent.mt,lower=F)
 
-    tbl0=tibble(
-        Feature=c("nCount_RNA","nFeature_RNA","percent.mt"),
-        Cutoff=c(MIN_NCOUNT_RNA,MIN_FEATURE_RNA,PCT_MITO),
-        Method=c("Mad3-","Mad3-","Mad3+")
+    filters=list(
+        MIN_NCOUNT_RNA=MIN_NCOUNT_RNA,
+        MIN_FEATURE_RNA=MIN_FEATURE_RNA,
+        PCT_MITO=PCT_MITO
     )
 
-    tbl=md %>%
-        count(
-            Count.RNA=nCount_RNA>MIN_NCOUNT_RNA,
-            Num.Features=nFeature_RNA>MIN_FEATURE_RNA,
-            PCT.MT=percent.mt<PCT_MITO
-            ) %>%
-        mutate(PCT=round(100*n/sum(n),1))
+    qTbls=get_qc_tables(md,filters)
 
-    nFail=c(
-        sum(md$nCount_RNA<=MIN_NCOUNT_RNA),
-        sum(md$nFeature_RNA<=MIN_FEATURE_RNA),
-        sum(md$percent.mt>=PCT_MITO)
-    )
-
-    pctFail=c(
-        mean(md$nCount_RNA<=MIN_NCOUNT_RNA),
-        mean(md$nFeature_RNA<=MIN_FEATURE_RNA),
-        mean(md$percent.mt>=PCT_MITO)
-    )
-
-    tblb=tibble(
-        Feature=c("nCount_RNA","nFeature_RNA","percent.mt"),
-        N.Fail=nFail,PCT.Fail=pctFail
-    )
-
-
-    tbl3a=md %>% group_by(SampleID) %>% summarize(Fail.Feature_RNA=mean(nFeature_RNA<=MIN_FEATURE_RNA),Fail.Count_RNA=mean(nCount_RNA<=MIN_NCOUNT_RNA),Fail.Mito=mean(percent.mt>=PCT_MITO))
-    tbl3b=md %>% group_by(SampleID) %>% summarize(Fail.Feature_RNA=sum(nFeature_RNA<=MIN_FEATURE_RNA),Fail.Count_RNA=sum(nCount_RNA<=MIN_NCOUNT_RNA),Fail.Mito=sum(percent.mt>=PCT_MITO))
-
-    ptb=list()
-    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tbl0,is.numeric,~round(.,2)),rows=NULL))
-    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tbl,is.numeric,~round(.,2)),rows=NULL))
-    tt=tblb %>% mutate(PCT.Fail=round(PCT.Fail*100,2))
-    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tt,is.numeric,~round(.,2)),rows=NULL))
-    tt=tbl3a %>% mutate_if(is.numeric,\(x)x*100)
-    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tt,is.numeric,~round(.,2)),rows=NULL))
-    ptb[[len(ptb)+1]]=ggplot()+theme_void()+annotation_custom(tableGrob(mutate_if(tbl3b,is.numeric,~round(.,2)),rows=NULL))
+    ptb=plot_qTables(qTbls)
 
     pq[[len(pq)+1]]=(ptb[[1]]+ptb[[2]]+ptb[[3]])/(ptb[[4]]+ptb[[5]])
 
-    list(plts=pq, stats=list(tbl0,tbl,tblb,tbl3a,tbl3b))
+    list(plts=pq, stats=qTbls)
 
 }
 
