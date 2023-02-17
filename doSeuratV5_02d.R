3cd63482d4772880c90eb7217c4167534c2d14a9
Nicholas D. Socci <soccin@mskcc.org>
2023-02-17 15:32:20 -0500

    Fix plot numbers for gene markers

--- doSeuratV5_02d.R
+++ doSeuratV5_02d.R
@@ -58,7 +58,7 @@ source(file.path(SDIR,"plotTools.R"))
 glbs=args$glbs
 ap=args$algoParams
 
-plotNo<-makeAutoIncrementor(20)
+plotNo<-makeAutoIncrementor(30)
 
 ##########################################################################
 #
@@ -130,7 +130,7 @@ pal1=c(cols25(25),brewer.dark2(8))
 
 plt.cmark=cl %>% group_split(cluster) %>% map(plotClusterMarkers,s1,pal1)
 
-cmFile=cc("seuratQC",args$PROJNAME,paste0(plotNo(),"d"),"ClusterMarkers",clustTag,"FDR",FDR.cut,"logFC",logFC.cut,"%03d",".png")
+cmFile=cc("seuratQC",args$PROJNAME,plotNo(),"ClusterMarkers",clustTag,"FDR",FDR.cut,"logFC",logFC.cut,"%03d",".png")
 
 png(filename=cmFile,
     type="cairo",
@@ -167,7 +167,7 @@ halt("Fix x -axis")
 pc1=DotPlot(s1,features=dot.gene.lOR) + scale_x_discrete(guide = guide_axis(n.dodge = 2))
 pc2=DotPlot(s1,features=dot.gene.lFC) + scale_x_discrete(guide = guide_axis(n.dodge = 2))
 
-pdf(file=cc("seuratQC",args$PROJNAME,paste0(plotNo(),"c"),"ClusterMarkersDot",clustTag,"FDR",FDR.cut,"logFC",logFC.cut,".pdf"),width=11,height=8.5)
+pdf(file=cc("seuratQC",args$PROJNAME,plotNo(),"ClusterMarkersDot",clustTag,"FDR",FDR.cut,"logFC",logFC.cut,".pdf"),width=11,height=8.5)
 print(pc1)
 print(pc2)
 dev.off()
