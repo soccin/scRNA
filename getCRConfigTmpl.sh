00e4625ab5bc3877029f754f1c24c97c7b94e5c7
Nicholas D. Socci <soccin@mskcc.org>
2023-06-24 03:52:15 -0400

    Sort samples

--- getCRConfigTmpl.sh
+++ getCRConfigTmpl.sh
@@ -2,10 +2,12 @@
 
 CELLRANGER=$1
 
-OUTS=$(find $CELLRANGER -type d | egrep "/outs$" | xargs realpath)
+OUTS=$(find $CELLRANGER -type d | egrep "/outs$" | xargs realpath | sort -V)
 
+echo "inputs:"
 for oi in $OUTS; do
     si=$(echo $oi | sed 's/.outs$//' | tr '/' '\n' | tail -1)
     echo "- dir:" $oi
     echo "  sid:" $si
 done
+echo "genome: [mm10|human?]"
