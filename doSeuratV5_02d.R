9d7f2d03a5c41fd26fd9ac259cce22cc8b4b8b5f
Nicholas D. Socci <soccin@mskcc.org>
2024-09-29 15:32:53 -0400

    Added cluster pathway analysis

--- doSeuratV5_02d.R
+++ doSeuratV5_02d.R
@@ -228,3 +228,66 @@ print(pgu)
 
 dev.off()
 mergePNGs(umFile)
+fs::file_delete(fs::dir_ls(dirname(umFile),regex="\\.png$"))
+
+#
+# Redo marker analysis with no threshold to get lists for pathway analysis
+#
+
+cat("\n\n\tDoing FindAllMarkers for Pathway analysis\n\tThis will take a long time\n\n")
+
+clusterMarkersForPathways=FindAllMarkers(s1,only.pos=F,logfc.threshold=0,min.pct = 0.25)
+
+write_csv(clusterMarkersForPathways,cc(args$PROJNAME,"TblClusterMarkers",clustTag,"PathwayList.csv"))
+
+library(fgsea)
+library(msigdbr)
+
+if(args$glbs$genome %in% c("human","xenograft")) {
+
+    msigdb_species="Homo sapiens"
+
+} else if(args$glbs$genome %in% c("mm10")) {
+
+    msigdb_species="Mus musculus"
+
+} else {
+
+    cat("\n\nUnknown genome for msigdb",args$glbs$genome,"\n\n")
+    rlang::abort("FATAL::ERROR")
+
+}
+
+msigdbr_df = msigdbr(species = msigdb_species)
+msigdbr_list = split(x = msigdbr_df$gene_symbol, f = msigdbr_df$gs_name)
+pathdb=msigdbr_df %>% distinct(gs_id,.keep_all=T) %>% select(-gene_symbol,-entrez_gene,-ensembl_gene,-human_gene_symbol,-human_entrez_gene,-human_ensembl_gene)
+
+pathways=list()
+qCutPath=0.05
+
+cmp=split(clusterMarkersForPathways,clusterMarkersForPathways$cluster)
+
+for(ci in cmp) {
+
+    cluster=as.character(ci$cluster[1])
+    gstats=ci$avg_log2FC
+    names(gstats)=ci$gene
+    fg=fgsea(msigdbr_list,gstats,minSize=15,maxSize=500)
+
+    pt=tibble(fg) %>%
+        filter(padj<qCutPath) %>%
+        arrange(pval) %>%
+        rowwise %>%
+        mutate(leadingEdge=paste((leadingEdge),collapse=";")) %>%
+        left_join(pathdb,by=c(pathway="gs_name"))
+
+    pathways[[cluster]]=pt
+
+}
+
+pt_df=map(pathways,data.frame)
+
+openxlsx::write.xlsx(
+                pt_df,
+                cc(args$PROJNAME,"ClusterMarkerPathways",clustTag,"FDR",qCutPath,"V2.xlsx")
+            )
