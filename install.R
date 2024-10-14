4aa344c713a90e168f181d5eb5cfc05e05775a50
Nicholas D. Socci <soccin@mskcc.org>
2024-10-14 17:18:53 -0400

    Working conda install for celldex

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("openxlsx")
+install.packages("pals")
+install.packages("ggpubr")
+BiocManager::install('limma')
+#install.packages("BiocManager")
+#BiocManager::install("celldex")
+#BiocManager::install("SingleR")
+
